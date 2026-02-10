2521223f4f45f116c1ebd6e42a60ef0e9faaf3e9
cibely07 <cibelyluizaaluna@gmail.com>
2026-02-10 10:21:58 -0300

    Update funcao_curl.php

--- fipe/funcao_curl.php
+++ fipe/funcao_curl.php
@@ -5,9 +5,9 @@ function consultarAPI($url) {
     
    
     curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); // Retorna o resultado como string [cite: 2]
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // Apenas para teste local [cite: 2]
-    curl_setopt($ch, CURLOPT_TIMEOUT, 10); // Máximo 10 segundos [cite: 2]
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); 
+    curl_setopt($ch, CURLOPT_TIMEOUT, 10); 
 
     $resposta = curl_exec($ch);
     $erro = curl_error($ch);
@@ -34,4 +34,5 @@ function consultarAPI($url) {
 
     return $dados;
 }
+
 ?>
